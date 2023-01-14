0cf8b38b8b0ab40943eb1fc82a60e7d8c0e48e0e
homero <homerogazze1015@gmail.com>
2023-01-14 12:18:23 -0300

    [FT/003] ./components - Projects container created

---
+++ src/components/homePage/projectsContainer.jsx
@@ -0,0 +1,47 @@
+// react imports
+import React, { useState, useEffect } from 'react'
+
+// Material UI imports
+import { Box } from '@mui/material'
+
+// Components imports
+import ProjectCard from '../ProyectCard/projectCard.jsx'
+
+// Others imports
+import HeaderConstructor from '../../utils/constructors/headerConstructor.js'
+import axios from 'axios'
+
+// Css imports
+import styles from './projectsContainer.module.css'
+
+export default function ProjectsContainer () {
+  const [projects, setProjects] = useState([])
+  const [totalPages, setTotalPages] = useState(0)
+
+  const getProjects = async (page) => {
+    try {
+      const config = HeaderConstructor()
+
+      const response = await axios.get(`/projects?page=${page}&limit=6`, config)
+
+      setProjects(response.data.body.projects)
+      setTotalPages(response.data.body.totalPages)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getProjects(1)
+  }, [])
+
+  return (
+    <Box className={styles.high_container}>
+      <Box className={styles.mid_container}>
+        {projects.map(p => (
+          <ProjectCard key={p._id} id={p._id} name={p.name} tag={p.tag} />
+        ))}
+      </Box>
+    </Box>
+  )
+}
