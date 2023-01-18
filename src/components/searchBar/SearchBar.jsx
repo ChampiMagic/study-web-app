1a7858fccf9c6dd2f77596738a687addd6bd5d44
augustobor <augus_borromeo@hotmail.com>
2023-01-18 11:21:02 -0300

    [FT/009] src/components/searchBar Making changes acording the FT/009 MR feedback

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,19 @@
 // react imports
 import { React, useState } from 'react'
 
-// Assets import
-import searchIcon from '../../assets/bx-search.svg'
+// @mui imports
+import SearchIcon from '@mui/icons-material/Search'
+import IconButton from '@mui/material/IconButton'
 
 // Css import
-import styles from './navigator.module.css'
+import styles from './searchBar.module.css'
 
 // Formik imports
 import { Field, Form, Formik } from 'formik'
 
 // axios import
 import axios from 'axios'
+import HeaderConstructor from '../../utils/constructors/headerConstructor'
 
 export default function SearchBar ({ isProjectSearchBar }) {
   const [statusMessage, setStatusMessage] = useState('')
@@ -22,12 +24,11 @@ export default function SearchBar ({ isProjectSearchBar }) {
 
   const handleSubmit = async ({ searchValue }, reset) => {
     try {
-      const URL = isProjectSearchBar ? 'proyectsByName route here' : 'cardsByName route here' // TODO!!
-      const response = await axios.get(URL, {
-        params: {
-          name: searchValue
-        }
-      })
+      const myHeader = HeaderConstructor()
+      const URL = isProjectSearchBar
+        ? 'proyectsByName with params route here'
+        : 'cardsByName with params route here' // TODO!!
+      const response = await axios.get(URL, myHeader)
       console.log(response)
       // TODO Send response to searchBar globalState here!
       reset()
@@ -53,9 +54,9 @@ export default function SearchBar ({ isProjectSearchBar }) {
               placeholder='Search Here!'
             />
           </div>
-          <button type='submit' className={styles.searchBarButton}>
-            <img src={searchIcon} alt='searchBar logo' />
-          </button>
+          <IconButton aria-label='search' type='submit' alt='searchBar logo'>
+            <SearchIcon />
+          </IconButton>
           {statusMessage ? <p className={styles.statusError}>{statusMessage}</p> : null}
         </Form>
       </Formik>
