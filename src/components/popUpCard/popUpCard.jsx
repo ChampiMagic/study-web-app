dc5570c8be71ec5460ce5cc58a112c7332dd8315
augustobor <augus_borromeo@hotmail.com>
2023-02-25 17:31:03 -0300

    [FT/016] cambios realizados

--- src/components/popUpCard/popUpCard.jsx
+++ src/components/popUpCard/popUpCard.jsx
@@ -1,61 +1,98 @@
 // React imports
-import * as React from "react";
+import * as React from 'react'
 // MUI imports
-import Button from "@mui/joy/Button";
-import Input from "@mui/joy/Input";
-import Modal from "@mui/joy/Modal";
-import ModalDialog from "@mui/joy/ModalDialog";
-import Typography from "@mui/joy/Typography";
+import Button from '@mui/joy/Button'
+import Input from '@mui/joy/Input'
+import Modal from '@mui/joy/Modal'
+import ModalDialog from '@mui/joy/ModalDialog'
+import Typography from '@mui/joy/Typography'
 
-// Css imports
-import styles from "./popUpCard.module.css";
+// axios import
+import HeaderConstructor from '../../utils/constructors/headerConstructor'
+import axios from 'axios'
+import { changeSelectedProject } from '../../redux/slices/projectSlice'
 
-export default function PopUpCard({ question }) {
-  const [open, setOpen] = React.useState(true);
+import { useDispatch } from 'react-redux'
+
+export default function PopUpCard ({ open, setOpen, question, projectId, id }) {
+  const dispatch = useDispatch()
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleSubmit = async () => {
+    try {
+      setOpen(false)
+      const config = HeaderConstructor()
+      const body = {
+        cardId: id,
+        projectId,
+        isCorrect: true
+      }
+
+      const response = await axios.put('/move-card', body, config)
+
+      dispatch(changeSelectedProject(response.data.body))
+
+      handleClose()
+    } catch (e) {
+      console.error('[Error en la llamada a move-card] ' + e)
+    }
+  }
+
   return (
     <Modal open={open}>
       <ModalDialog
         sx={{
           maxWidth: 500,
-          borderRadius: "md",
+          borderRadius: 'md',
           p: 3,
-          boxShadow: "lg",
+          boxShadow: 'lg',
+          backgroundColor: '#FAFAFA',
+          color: '#1c1c1c'
         }}
       >
         <Typography
-          component="h2"        
-          fontSize="1.25em"
-          mb="0.25em"
+          component='h2'
+          fontSize='1.25em'
+          mb='0.25em'
         >
           La pregunta de hoy es:
         </Typography>
         <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            setOpen(false);
+          onSubmit={() => {
+            handleSubmit()
           }}
         >
           <Typography
-            variant="h1"
-            component="h2"
-            align="center"
-            style={{ wordWrap: "break-word" }}
-            >
+            component='h2'
+            align='center'
+            style={{ wordWrap: 'break-word' }}
+          >
             {/* En esta parte se muestra la pregunta que pasamos por prop */}
             {question}
           </Typography>
 
           <Input
-            aria-label="empty textarea"
-            placeholder="Tu respuesta es"
+            aria-label='empty textarea'
+            placeholder='Tu respuesta es'
             style={{ minWidth: 300 }}
           />
-          <br></br>
-          <Button variant="outlined" color="primary" type="submit" >
+          <br />
+          <Button
+            variant='outlined' color='primary' type='submit' sx={{
+              transition: 'all 400ms ease',
+              ':hover': {
+                backgroundColor: 'tomato',
+                transition: 'all 400ms ease'
+              }
+            }}
+          >
             Enviar
           </Button>
         </form>
       </ModalDialog>
     </Modal>
-  );
+  )
 }
